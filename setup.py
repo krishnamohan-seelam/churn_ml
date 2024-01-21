9bea113968cfad471f2d5a2f7cbf9300d0a80e77
krishnamohan-seelam <krishnamohan.seelam@outlook.com>
2024-01-21 11:48:35 +0530

    Adding cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='E2E ML pipeline with MLOps tools',
+    author='KrishnaMohan Seelam',
+    license='MIT',
+)
